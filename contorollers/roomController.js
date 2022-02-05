298cae8603a62ccb959624b7939971a8a21f577a
imyr <imyrgim92@gmail.com>
2022-02-05 17:36:02 +0900

    refactor: Change code res.json data

--- contorollers/roomController.js
+++ contorollers/roomController.js
@@ -3,7 +3,7 @@ const roomService = require("../services/roomService");
 exports.getRooms = async (req, res, next) => {
   // const rooms = await roomService.getRooms();
   // res.json({ rooms });
-  res.json({ success: "room datas" });
+  res.json({ success: "room data" });
 };
 
 exports.createRoom = async (req, res, next) => {};
