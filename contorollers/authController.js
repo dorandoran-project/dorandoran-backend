070dadb1f32e8c6121f37046a34800cbf622135c
so098 <szerohan@gmail.com>
2022-02-05 22:14:39 +0900

    refactor: Change authService

--- contorollers/authController.js
+++ contorollers/authController.js
@@ -3,41 +3,48 @@ const authService = require("../services/authService");
 const roomService = require("../services/roomService");
 const communityService = require("../services/communityService");
 const constants = require("../utils/constants");
-const User = require("../models/User");
 
 exports.login = async (req, res) => {
-  const { name, email, profile, age_range, gender } = req.body;
+  try {
+    const userInfo = req.body;
 
-  if (!email) {
-    return res.json("이메일 항목에 동의해주세요"); //테스트 중입니다
-  }
+    if (!userInfo.email) {
+      return res.json("이메일 항목에 동의해주세요"); //테스트 중입니다
+    }
 
-  const accessToken = jwt.sign({ email: email }, process.env.COOKIE_SECRET, {
-    expiresIn: "1h", //토큰 만료 시간
-  });
+    const accessToken = jwt.sign(
+      { email: userInfo.email },
+      process.env.JWT_ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
-  const refreshToken = jwt.sign({ email: email }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+    const refreshToken = jwt.sign(
+      { email: userInfo.email },
+      process.env.JWT_REFRESH_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
 
-  res.cookie("access_token", accessToken, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000,
-  });
-
-  const user = await authService.getUser(email, refreshToken);
-  if (!user) {
-    await User.create({
-      name,
-      email,
-      profile,
-      age_range,
-      gender,
-      refresh_token: refreshToken,
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      maxAge: 3600,
     });
-  }
 
-  res.status(200).json(user);
+    let user = await authService.getUser(userInfo.email);
+
+    if (!user) {
+      user = await authService.createUser(userInfo, refreshToken);
+    } else {
+      await authService.saveRefreshToken(user, refreshToken);
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.refresh = async (req, res) => {};
--- services/authService.js
+++ services/authService.js
@@ -1,9 +1,21 @@
 const User = require("../models/User");
 
-exports.getUser = async (email, refreshToken) => {
-  const user = await User.findOne({ email });
+exports.getUser = async (email) => {
+  return await User.findOne({ email });
+};
+
+exports.createUser = async (userInfo, refreshToken) => {
+  return await User.create({
+    name: userInfo.name,
+    email: userInfo.email,
+    profile: userInfo.profile,
+    age_range: userInfo.age_range,
+    gender: userInfo.gender,
+    refresh_token: refreshToken,
+  });
+};
+
+exports.saveRefreshToken = async (user, refreshToken) => {
   user.refresh_token = refreshToken;
   await user.save();
-
-  return user;
 };
