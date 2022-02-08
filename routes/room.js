4d975bc55eaf5414eaa0d67af5309850ebadb927
seodongsu <xl445566@naver.com>
2022-02-08 23:40:01 +0900

    feat: Add room router Joi validation

--- routes/room.js
+++ routes/room.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const roomController = require("../contorollers/roomController");
+const {
+  validateRoom,
+  validateRefreshRoom,
+  validateNewRoom,
+} = require("../middlewares/validate");
+const { isNotLoggedIn } = require("../middlewares/isLogged");
 
-router.get("/", roomController.init);
+router.get("/", isNotLoggedIn, roomController.init);
 
-router.post("/", roomController.getRooms);
+router.post("/", validateRoom, isNotLoggedIn, roomController.getRooms);
 
-router.post("/refresh", roomController.reload);
+router.post(
+  "/refresh",
+  isNotLoggedIn,
+  validateRefreshRoom,
+  roomController.reload
+);
 
-router.post("/new", roomController.createRoom);
+router.post("/new", validateNewRoom, isNotLoggedIn, roomController.createRoom);
 
 module.exports = router;
